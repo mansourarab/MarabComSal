936798a37b0970cf5568fc62a16819b56d75c3ec
Mansour Arab <mansour.t.arab@gmail.com>
2021-04-24 16:39:08 +0300

    Initiate EmployeeLogin Form Code

--- MarabComSal/Form_EmployeeLogin.cs
+++ MarabComSal/Form_EmployeeLogin.cs
@@ -16,5 +16,33 @@ namespace MarabComSal
         {
             InitializeComponent();
         }
+
+        private void Form_EmployeeLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Login_Click(object sender, EventArgs e)
+        {
+            EmployeeLoginClass elogin = new EmployeeLoginClass();
+            int? AccountId;
+            int result = elogin.CheckUser(Tb_Username.Text, Tb_Password.Text, out AccountId);
+            if (result == 1)
+            {
+                Lbl_Status.ForeColor = System.Drawing.Color.Green;
+                Lbl_Status.Text = "Loggied in Successfully!";
+
+                EmployeeLoginClass.AccountId = AccountId;
+
+                Home Hform = new Home();
+                Hform.Show();
+
+            }
+            else
+            {
+                Lbl_Status.ForeColor = System.Drawing.Color.Red;
+                Lbl_Status.Text = "Invalid Info";
+            }
+        }
     }
 }
