e371111b02344755379e3136632f99a04167cc1f
Mansour Arab <mansour.t.arab@gmail.com>
2021-04-24 16:31:51 +0300

    Start up page is UserInfo

--- MarabComSal/Program.cs
+++ MarabComSal/Program.cs
@@ -16,7 +16,7 @@ namespace MarabComSal
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form_UserTypeInfo());
         }
     }
 }
