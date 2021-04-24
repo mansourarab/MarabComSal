f1d3d7c29f931ec6331861b556725618aeac4c09
Mansour Arab <mansour.t.arab@gmail.com>
2021-04-24 16:23:08 +0300

    Initiate EmployeeNewAccount Form

---
+++ MarabComSal/Form_EmployeeNewAccount.Designer.cs
@@ -0,0 +1,325 @@
+﻿
+namespace MarabComSal
+{
+    partial class Form_EmployeeNewAccount
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Gb_Createnewemp = new System.Windows.Forms.GroupBox();
+            this.Btn_Back = new System.Windows.Forms.Button();
+            this.Btn_Checkavailability = new System.Windows.Forms.Button();
+            this.lbl_status = new System.Windows.Forms.Label();
+            this.Btn_Create = new System.Windows.Forms.Button();
+            this.Tb_Phone = new System.Windows.Forms.TextBox();
+            this.Tb_Address = new System.Windows.Forms.TextBox();
+            this.Tb_Email = new System.Windows.Forms.TextBox();
+            this.Tb_Password = new System.Windows.Forms.TextBox();
+            this.Tb_Username = new System.Windows.Forms.TextBox();
+            this.Tb_Fullname = new System.Windows.Forms.TextBox();
+            this.Rbtn_Female = new System.Windows.Forms.RadioButton();
+            this.Lbl_Gender = new System.Windows.Forms.Label();
+            this.Rbtn_Male = new System.Windows.Forms.RadioButton();
+            this.Lbl_Phone = new System.Windows.Forms.Label();
+            this.Lbl_Address = new System.Windows.Forms.Label();
+            this.Lbl_Email = new System.Windows.Forms.Label();
+            this.Lbl_Password = new System.Windows.Forms.Label();
+            this.Lbl_Username = new System.Windows.Forms.Label();
+            this.Lbl_Fullname = new System.Windows.Forms.Label();
+            this.Tb_Department = new System.Windows.Forms.TextBox();
+            this.Lbl_Department = new System.Windows.Forms.Label();
+            this.Gb_Createnewemp.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Gb_Createnewemp
+            // 
+            this.Gb_Createnewemp.Controls.Add(this.Tb_Department);
+            this.Gb_Createnewemp.Controls.Add(this.Lbl_Department);
+            this.Gb_Createnewemp.Controls.Add(this.Btn_Back);
+            this.Gb_Createnewemp.Controls.Add(this.Btn_Checkavailability);
+            this.Gb_Createnewemp.Controls.Add(this.lbl_status);
+            this.Gb_Createnewemp.Controls.Add(this.Btn_Create);
+            this.Gb_Createnewemp.Controls.Add(this.Tb_Phone);
+            this.Gb_Createnewemp.Controls.Add(this.Tb_Address);
+            this.Gb_Createnewemp.Controls.Add(this.Tb_Email);
+            this.Gb_Createnewemp.Controls.Add(this.Tb_Password);
+            this.Gb_Createnewemp.Controls.Add(this.Tb_Username);
+            this.Gb_Createnewemp.Controls.Add(this.Tb_Fullname);
+            this.Gb_Createnewemp.Controls.Add(this.Rbtn_Female);
+            this.Gb_Createnewemp.Controls.Add(this.Lbl_Gender);
+            this.Gb_Createnewemp.Controls.Add(this.Rbtn_Male);
+            this.Gb_Createnewemp.Controls.Add(this.Lbl_Phone);
+            this.Gb_Createnewemp.Controls.Add(this.Lbl_Address);
+            this.Gb_Createnewemp.Controls.Add(this.Lbl_Email);
+            this.Gb_Createnewemp.Controls.Add(this.Lbl_Password);
+            this.Gb_Createnewemp.Controls.Add(this.Lbl_Username);
+            this.Gb_Createnewemp.Controls.Add(this.Lbl_Fullname);
+            this.Gb_Createnewemp.Font = new System.Drawing.Font("Tahoma", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.Gb_Createnewemp.Location = new System.Drawing.Point(213, 28);
+            this.Gb_Createnewemp.Name = "Gb_Createnewemp";
+            this.Gb_Createnewemp.Size = new System.Drawing.Size(365, 592);
+            this.Gb_Createnewemp.TabIndex = 0;
+            this.Gb_Createnewemp.TabStop = false;
+            this.Gb_Createnewemp.Text = "Create New Employee Account";
+            // 
+            // Btn_Back
+            // 
+            this.Btn_Back.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.Btn_Back.Location = new System.Drawing.Point(18, 485);
+            this.Btn_Back.Name = "Btn_Back";
+            this.Btn_Back.Size = new System.Drawing.Size(125, 27);
+            this.Btn_Back.TabIndex = 37;
+            this.Btn_Back.Text = "Back to Login";
+            this.Btn_Back.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Checkavailability
+            // 
+            this.Btn_Checkavailability.Font = new System.Drawing.Font("Tahoma", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.Btn_Checkavailability.Location = new System.Drawing.Point(18, 422);
+            this.Btn_Checkavailability.Name = "Btn_Checkavailability";
+            this.Btn_Checkavailability.Size = new System.Drawing.Size(167, 30);
+            this.Btn_Checkavailability.TabIndex = 36;
+            this.Btn_Checkavailability.Text = "Check Availability!";
+            this.Btn_Checkavailability.UseVisualStyleBackColor = true;
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.AutoSize = true;
+            this.lbl_status.Location = new System.Drawing.Point(143, 463);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(0, 23);
+            this.lbl_status.TabIndex = 35;
+            // 
+            // Btn_Create
+            // 
+            this.Btn_Create.Font = new System.Drawing.Font("Tahoma", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.Btn_Create.Location = new System.Drawing.Point(233, 422);
+            this.Btn_Create.Name = "Btn_Create";
+            this.Btn_Create.Size = new System.Drawing.Size(125, 30);
+            this.Btn_Create.TabIndex = 34;
+            this.Btn_Create.Text = "Create";
+            this.Btn_Create.UseVisualStyleBackColor = true;
+            // 
+            // Tb_Phone
+            // 
+            this.Tb_Phone.Font = new System.Drawing.Font("Courier New", 9F);
+            this.Tb_Phone.Location = new System.Drawing.Point(111, 280);
+            this.Tb_Phone.Name = "Tb_Phone";
+            this.Tb_Phone.Size = new System.Drawing.Size(186, 21);
+            this.Tb_Phone.TabIndex = 33;
+            // 
+            // Tb_Address
+            // 
+            this.Tb_Address.Font = new System.Drawing.Font("Courier New", 9F);
+            this.Tb_Address.Location = new System.Drawing.Point(111, 239);
+            this.Tb_Address.Name = "Tb_Address";
+            this.Tb_Address.Size = new System.Drawing.Size(186, 21);
+            this.Tb_Address.TabIndex = 32;
+            // 
+            // Tb_Email
+            // 
+            this.Tb_Email.Font = new System.Drawing.Font("Courier New", 9F);
+            this.Tb_Email.Location = new System.Drawing.Point(111, 166);
+            this.Tb_Email.Name = "Tb_Email";
+            this.Tb_Email.Size = new System.Drawing.Size(186, 21);
+            this.Tb_Email.TabIndex = 31;
+            // 
+            // Tb_Password
+            // 
+            this.Tb_Password.Font = new System.Drawing.Font("Courier New", 9F);
+            this.Tb_Password.Location = new System.Drawing.Point(111, 126);
+            this.Tb_Password.Name = "Tb_Password";
+            this.Tb_Password.Size = new System.Drawing.Size(186, 21);
+            this.Tb_Password.TabIndex = 30;
+            // 
+            // Tb_Username
+            // 
+            this.Tb_Username.Font = new System.Drawing.Font("Courier New", 9F);
+            this.Tb_Username.Location = new System.Drawing.Point(111, 85);
+            this.Tb_Username.Name = "Tb_Username";
+            this.Tb_Username.Size = new System.Drawing.Size(186, 21);
+            this.Tb_Username.TabIndex = 29;
+            // 
+            // Tb_Fullname
+            // 
+            this.Tb_Fullname.Font = new System.Drawing.Font("Courier New", 9F);
+            this.Tb_Fullname.Location = new System.Drawing.Point(111, 48);
+            this.Tb_Fullname.Name = "Tb_Fullname";
+            this.Tb_Fullname.Size = new System.Drawing.Size(186, 21);
+            this.Tb_Fullname.TabIndex = 28;
+            // 
+            // Rbtn_Female
+            // 
+            this.Rbtn_Female.AutoSize = true;
+            this.Rbtn_Female.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Underline);
+            this.Rbtn_Female.Location = new System.Drawing.Point(159, 365);
+            this.Rbtn_Female.Name = "Rbtn_Female";
+            this.Rbtn_Female.Size = new System.Drawing.Size(77, 23);
+            this.Rbtn_Female.TabIndex = 27;
+            this.Rbtn_Female.TabStop = true;
+            this.Rbtn_Female.Text = "Female";
+            this.Rbtn_Female.UseVisualStyleBackColor = true;
+            // 
+            // Lbl_Gender
+            // 
+            this.Lbl_Gender.AutoSize = true;
+            this.Lbl_Gender.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.Lbl_Gender.Location = new System.Drawing.Point(7, 320);
+            this.Lbl_Gender.Name = "Lbl_Gender";
+            this.Lbl_Gender.Size = new System.Drawing.Size(66, 19);
+            this.Lbl_Gender.TabIndex = 26;
+            this.Lbl_Gender.Text = "Gender:";
+            // 
+            // Rbtn_Male
+            // 
+            this.Rbtn_Male.AutoSize = true;
+            this.Rbtn_Male.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Underline);
+            this.Rbtn_Male.Location = new System.Drawing.Point(11, 365);
+            this.Rbtn_Male.Name = "Rbtn_Male";
+            this.Rbtn_Male.Size = new System.Drawing.Size(59, 23);
+            this.Rbtn_Male.TabIndex = 25;
+            this.Rbtn_Male.TabStop = true;
+            this.Rbtn_Male.Text = "Male";
+            this.Rbtn_Male.UseVisualStyleBackColor = true;
+            // 
+            // Lbl_Phone
+            // 
+            this.Lbl_Phone.AutoSize = true;
+            this.Lbl_Phone.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.Lbl_Phone.Location = new System.Drawing.Point(7, 282);
+            this.Lbl_Phone.Name = "Lbl_Phone";
+            this.Lbl_Phone.Size = new System.Drawing.Size(59, 19);
+            this.Lbl_Phone.TabIndex = 24;
+            this.Lbl_Phone.Text = "Phone:";
+            // 
+            // Lbl_Address
+            // 
+            this.Lbl_Address.AutoSize = true;
+            this.Lbl_Address.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.Lbl_Address.Location = new System.Drawing.Point(7, 239);
+            this.Lbl_Address.Name = "Lbl_Address";
+            this.Lbl_Address.Size = new System.Drawing.Size(72, 19);
+            this.Lbl_Address.TabIndex = 23;
+            this.Lbl_Address.Text = "Address:";
+            // 
+            // Lbl_Email
+            // 
+            this.Lbl_Email.AutoSize = true;
+            this.Lbl_Email.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.Lbl_Email.Location = new System.Drawing.Point(7, 165);
+            this.Lbl_Email.Name = "Lbl_Email";
+            this.Lbl_Email.Size = new System.Drawing.Size(54, 19);
+            this.Lbl_Email.TabIndex = 22;
+            this.Lbl_Email.Text = "Email:";
+            // 
+            // Lbl_Password
+            // 
+            this.Lbl_Password.AutoSize = true;
+            this.Lbl_Password.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.Lbl_Password.Location = new System.Drawing.Point(7, 125);
+            this.Lbl_Password.Name = "Lbl_Password";
+            this.Lbl_Password.Size = new System.Drawing.Size(82, 19);
+            this.Lbl_Password.TabIndex = 21;
+            this.Lbl_Password.Text = "Password:";
+            // 
+            // Lbl_Username
+            // 
+            this.Lbl_Username.AutoSize = true;
+            this.Lbl_Username.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.Lbl_Username.Location = new System.Drawing.Point(7, 87);
+            this.Lbl_Username.Name = "Lbl_Username";
+            this.Lbl_Username.Size = new System.Drawing.Size(86, 19);
+            this.Lbl_Username.TabIndex = 20;
+            this.Lbl_Username.Text = "Username:";
+            // 
+            // Lbl_Fullname
+            // 
+            this.Lbl_Fullname.AutoSize = true;
+            this.Lbl_Fullname.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.Lbl_Fullname.Location = new System.Drawing.Point(7, 50);
+            this.Lbl_Fullname.Name = "Lbl_Fullname";
+            this.Lbl_Fullname.Size = new System.Drawing.Size(86, 19);
+            this.Lbl_Fullname.TabIndex = 19;
+            this.Lbl_Fullname.Text = "Full Name:";
+            // 
+            // Tb_Department
+            // 
+            this.Tb_Department.Font = new System.Drawing.Font("Courier New", 9F);
+            this.Tb_Department.Location = new System.Drawing.Point(111, 201);
+            this.Tb_Department.Name = "Tb_Department";
+            this.Tb_Department.Size = new System.Drawing.Size(186, 21);
+            this.Tb_Department.TabIndex = 39;
+            // 
+            // Lbl_Department
+            // 
+            this.Lbl_Department.AutoSize = true;
+            this.Lbl_Department.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.Lbl_Department.Location = new System.Drawing.Point(7, 201);
+            this.Lbl_Department.Name = "Lbl_Department";
+            this.Lbl_Department.Size = new System.Drawing.Size(98, 19);
+            this.Lbl_Department.TabIndex = 38;
+            this.Lbl_Department.Text = "Department:";
+            // 
+            // Form_EmployeeNewAccount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 632);
+            this.Controls.Add(this.Gb_Createnewemp);
+            this.Name = "Form_EmployeeNewAccount";
+            this.Text = "Form_EmployeeNewAccount";
+            this.Gb_Createnewemp.ResumeLayout(false);
+            this.Gb_Createnewemp.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Gb_Createnewemp;
+        private System.Windows.Forms.Button Btn_Back;
+        private System.Windows.Forms.Button Btn_Checkavailability;
+        private System.Windows.Forms.Label lbl_status;
+        private System.Windows.Forms.Button Btn_Create;
+        private System.Windows.Forms.TextBox Tb_Phone;
+        private System.Windows.Forms.TextBox Tb_Address;
+        private System.Windows.Forms.TextBox Tb_Email;
+        private System.Windows.Forms.TextBox Tb_Password;
+        private System.Windows.Forms.TextBox Tb_Username;
+        private System.Windows.Forms.TextBox Tb_Fullname;
+        private System.Windows.Forms.RadioButton Rbtn_Female;
+        private System.Windows.Forms.Label Lbl_Gender;
+        private System.Windows.Forms.RadioButton Rbtn_Male;
+        private System.Windows.Forms.Label Lbl_Phone;
+        private System.Windows.Forms.Label Lbl_Address;
+        private System.Windows.Forms.Label Lbl_Email;
+        private System.Windows.Forms.Label Lbl_Password;
+        private System.Windows.Forms.Label Lbl_Username;
+        private System.Windows.Forms.Label Lbl_Fullname;
+        private System.Windows.Forms.TextBox Tb_Department;
+        private System.Windows.Forms.Label Lbl_Department;
+    }
+}
