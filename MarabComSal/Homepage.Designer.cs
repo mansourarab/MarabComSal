4494a5aae89122b70d879be775ba978ed0485e4a
Mansour Arab <mansour.t.arab@gmail.com>
2021-04-24 21:40:33 +0300

    Initiate General Homepage

--- MarabComSal/Homepage.Designer.cs
+++ MarabComSal/Homepage.Designer.cs
@@ -29,13 +29,197 @@ namespace MarabComSal
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.myProfileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.updatePasswordEmailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ordersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contactInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logOutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modifyOrCancelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adminstratorAuthoritiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newEmployeeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageOrdersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.modifyAndCancelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.myProfileToolStripMenuItem,
+            this.ordersToolStripMenuItem,
+            this.adminstratorAuthoritiesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // myProfileToolStripMenuItem
+            // 
+            this.myProfileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewToolStripMenuItem1,
+            this.updateToolStripMenuItem1,
+            this.logOutToolStripMenuItem1});
+            this.myProfileToolStripMenuItem.Name = "myProfileToolStripMenuItem";
+            this.myProfileToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.myProfileToolStripMenuItem.Text = "My Profile";
+            // 
+            // viewToolStripMenuItem1
+            // 
+            this.viewToolStripMenuItem1.Name = "viewToolStripMenuItem1";
+            this.viewToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.viewToolStripMenuItem1.Text = "View";
+            // 
+            // updateToolStripMenuItem1
+            // 
+            this.updateToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.updatePasswordEmailToolStripMenuItem,
+            this.contactInfoToolStripMenuItem});
+            this.updateToolStripMenuItem1.Name = "updateToolStripMenuItem1";
+            this.updateToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.updateToolStripMenuItem1.Text = "Update";
+            // 
+            // updatePasswordEmailToolStripMenuItem
+            // 
+            this.updatePasswordEmailToolStripMenuItem.Name = "updatePasswordEmailToolStripMenuItem";
+            this.updatePasswordEmailToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.updatePasswordEmailToolStripMenuItem.Text = "Password and  Email";
+            // 
+            // ordersToolStripMenuItem
+            // 
+            this.ordersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.modifyOrCancelToolStripMenuItem});
+            this.ordersToolStripMenuItem.Name = "ordersToolStripMenuItem";
+            this.ordersToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.ordersToolStripMenuItem.Text = "Orders";
+            // 
+            // contactInfoToolStripMenuItem
+            // 
+            this.contactInfoToolStripMenuItem.Name = "contactInfoToolStripMenuItem";
+            this.contactInfoToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.contactInfoToolStripMenuItem.Text = "Contact Info";
+            // 
+            // logOutToolStripMenuItem1
+            // 
+            this.logOutToolStripMenuItem1.Name = "logOutToolStripMenuItem1";
+            this.logOutToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.logOutToolStripMenuItem1.Text = "Log Out";
+            // 
+            // createToolStripMenuItem
+            // 
+            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.createToolStripMenuItem.Text = "Create";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // modifyOrCancelToolStripMenuItem
+            // 
+            this.modifyOrCancelToolStripMenuItem.Name = "modifyOrCancelToolStripMenuItem";
+            this.modifyOrCancelToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.modifyOrCancelToolStripMenuItem.Text = "Modify or Cancel";
+            // 
+            // adminstratorAuthoritiesToolStripMenuItem
+            // 
+            this.adminstratorAuthoritiesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newEmployeeToolStripMenuItem,
+            this.manageOrdersToolStripMenuItem});
+            this.adminstratorAuthoritiesToolStripMenuItem.Name = "adminstratorAuthoritiesToolStripMenuItem";
+            this.adminstratorAuthoritiesToolStripMenuItem.Size = new System.Drawing.Size(150, 20);
+            this.adminstratorAuthoritiesToolStripMenuItem.Text = "Adminstrator Authorities";
+            // 
+            // newEmployeeToolStripMenuItem
+            // 
+            this.newEmployeeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createToolStripMenuItem1,
+            this.deleteToolStripMenuItem});
+            this.newEmployeeToolStripMenuItem.Name = "newEmployeeToolStripMenuItem";
+            this.newEmployeeToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.newEmployeeToolStripMenuItem.Text = "Employees Accounts";
+            // 
+            // manageOrdersToolStripMenuItem
+            // 
+            this.manageOrdersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createToolStripMenuItem2,
+            this.modifyAndCancelToolStripMenuItem});
+            this.manageOrdersToolStripMenuItem.Name = "manageOrdersToolStripMenuItem";
+            this.manageOrdersToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.manageOrdersToolStripMenuItem.Text = "Manage Orders";
+            // 
+            // createToolStripMenuItem1
+            // 
+            this.createToolStripMenuItem1.Name = "createToolStripMenuItem1";
+            this.createToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.createToolStripMenuItem1.Text = "Create";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            // 
+            // createToolStripMenuItem2
+            // 
+            this.createToolStripMenuItem2.Name = "createToolStripMenuItem2";
+            this.createToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.createToolStripMenuItem2.Text = "Create";
+            // 
+            // modifyAndCancelToolStripMenuItem
+            // 
+            this.modifyAndCancelToolStripMenuItem.Name = "modifyAndCancelToolStripMenuItem";
+            this.modifyAndCancelToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.modifyAndCancelToolStripMenuItem.Text = "Modify and Cancel";
+            // 
+            // Homepage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Homepage";
+            this.Text = "Marab Company Homepage";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem myProfileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ordersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem updateToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem updatePasswordEmailToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contactInfoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logOutToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modifyOrCancelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem adminstratorAuthoritiesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newEmployeeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageOrdersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem modifyAndCancelToolStripMenuItem;
     }
 }
 
