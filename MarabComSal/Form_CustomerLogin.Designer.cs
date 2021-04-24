83640863503bef4276d924ca4655359a8a7af052
Mansour Arab <mansour.t.arab@gmail.com>
2021-04-24 15:41:52 +0300

    Initiate CustomerLogin Design Form

---
+++ MarabComSal/Form_CustomerLogin.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace MarabComSal
+{
+    partial class Form_CustomerLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Gb_Cuslog = new System.Windows.Forms.GroupBox();
+            this.Linlbl_Newaccount = new System.Windows.Forms.LinkLabel();
+            this.lbl_status = new System.Windows.Forms.Label();
+            this.Btn_Login = new System.Windows.Forms.Button();
+            this.Tb_Password = new System.Windows.Forms.TextBox();
+            this.Tb_Username = new System.Windows.Forms.TextBox();
+            this.Lbl_Pleaseenter = new System.Windows.Forms.Label();
+            this.Lbl_Password = new System.Windows.Forms.Label();
+            this.Lbl_Username = new System.Windows.Forms.Label();
+            this.Btn_Back = new System.Windows.Forms.Button();
+            this.Gb_Cuslog.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Gb_Cuslog
+            // 
+            this.Gb_Cuslog.Controls.Add(this.Btn_Back);
+            this.Gb_Cuslog.Controls.Add(this.Linlbl_Newaccount);
+            this.Gb_Cuslog.Controls.Add(this.lbl_status);
+            this.Gb_Cuslog.Controls.Add(this.Btn_Login);
+            this.Gb_Cuslog.Controls.Add(this.Tb_Password);
+            this.Gb_Cuslog.Controls.Add(this.Tb_Username);
+            this.Gb_Cuslog.Controls.Add(this.Lbl_Pleaseenter);
+            this.Gb_Cuslog.Controls.Add(this.Lbl_Password);
+            this.Gb_Cuslog.Controls.Add(this.Lbl_Username);
+            this.Gb_Cuslog.Font = new System.Drawing.Font("Tahoma", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.Gb_Cuslog.Location = new System.Drawing.Point(178, 87);
+            this.Gb_Cuslog.Name = "Gb_Cuslog";
+            this.Gb_Cuslog.Size = new System.Drawing.Size(445, 277);
+            this.Gb_Cuslog.TabIndex = 2;
+            this.Gb_Cuslog.TabStop = false;
+            this.Gb_Cuslog.Text = "Customer Login Info";
+            // 
+            // Linlbl_Newaccount
+            // 
+            this.Linlbl_Newaccount.AutoSize = true;
+            this.Linlbl_Newaccount.Font = new System.Drawing.Font("Tahoma", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.Linlbl_Newaccount.Location = new System.Drawing.Point(331, 248);
+            this.Linlbl_Newaccount.Name = "Linlbl_Newaccount";
+            this.Linlbl_Newaccount.Size = new System.Drawing.Size(108, 17);
+            this.Linlbl_Newaccount.TabIndex = 7;
+            this.Linlbl_Newaccount.TabStop = true;
+            this.Linlbl_Newaccount.Text = "New Account?";
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.AutoSize = true;
+            this.lbl_status.Location = new System.Drawing.Point(204, 92);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(0, 23);
+            this.lbl_status.TabIndex = 6;
+            // 
+            // Btn_Login
+            // 
+            this.Btn_Login.Font = new System.Drawing.Font("Tahoma", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.Btn_Login.Location = new System.Drawing.Point(161, 207);
+            this.Btn_Login.Name = "Btn_Login";
+            this.Btn_Login.Size = new System.Drawing.Size(152, 28);
+            this.Btn_Login.TabIndex = 5;
+            this.Btn_Login.Text = "Log in!";
+            this.Btn_Login.UseVisualStyleBackColor = true;
+            // 
+            // Tb_Password
+            // 
+            this.Tb_Password.Location = new System.Drawing.Point(148, 162);
+            this.Tb_Password.Name = "Tb_Password";
+            this.Tb_Password.Size = new System.Drawing.Size(174, 30);
+            this.Tb_Password.TabIndex = 4;
+            // 
+            // Tb_Username
+            // 
+            this.Tb_Username.Location = new System.Drawing.Point(148, 118);
+            this.Tb_Username.Name = "Tb_Username";
+            this.Tb_Username.Size = new System.Drawing.Size(174, 30);
+            this.Tb_Username.TabIndex = 3;
+            // 
+            // Lbl_Pleaseenter
+            // 
+            this.Lbl_Pleaseenter.AutoSize = true;
+            this.Lbl_Pleaseenter.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.Lbl_Pleaseenter.Location = new System.Drawing.Point(44, 68);
+            this.Lbl_Pleaseenter.Name = "Lbl_Pleaseenter";
+            this.Lbl_Pleaseenter.Size = new System.Drawing.Size(278, 19);
+            this.Lbl_Pleaseenter.TabIndex = 2;
+            this.Lbl_Pleaseenter.Text = "Please enter your credintials for login:";
+            // 
+            // Lbl_Password
+            // 
+            this.Lbl_Password.AutoSize = true;
+            this.Lbl_Password.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
+            this.Lbl_Password.Location = new System.Drawing.Point(44, 168);
+            this.Lbl_Password.Name = "Lbl_Password";
+            this.Lbl_Password.Size = new System.Drawing.Size(93, 19);
+            this.Lbl_Password.TabIndex = 1;
+            this.Lbl_Password.Text = "Password:";
+            // 
+            // Lbl_Username
+            // 
+            this.Lbl_Username.AutoSize = true;
+            this.Lbl_Username.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
+            this.Lbl_Username.Location = new System.Drawing.Point(44, 120);
+            this.Lbl_Username.Name = "Lbl_Username";
+            this.Lbl_Username.Size = new System.Drawing.Size(97, 19);
+            this.Lbl_Username.TabIndex = 0;
+            this.Lbl_Username.Text = "Username:";
+            // 
+            // Btn_Back
+            // 
+            this.Btn_Back.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Underline);
+            this.Btn_Back.Location = new System.Drawing.Point(6, 248);
+            this.Btn_Back.Name = "Btn_Back";
+            this.Btn_Back.Size = new System.Drawing.Size(73, 23);
+            this.Btn_Back.TabIndex = 8;
+            this.Btn_Back.Text = "Back";
+            this.Btn_Back.UseVisualStyleBackColor = true;
+            // 
+            // Form_CustomerLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Gb_Cuslog);
+            this.Name = "Form_CustomerLogin";
+            this.Text = "Form_CustomerNewAccount";
+            this.Gb_Cuslog.ResumeLayout(false);
+            this.Gb_Cuslog.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Gb_Cuslog;
+        private System.Windows.Forms.LinkLabel Linlbl_Newaccount;
+        private System.Windows.Forms.Label lbl_status;
+        private System.Windows.Forms.Button Btn_Login;
+        private System.Windows.Forms.TextBox Tb_Password;
+        private System.Windows.Forms.TextBox Tb_Username;
+        private System.Windows.Forms.Label Lbl_Pleaseenter;
+        private System.Windows.Forms.Label Lbl_Password;
+        private System.Windows.Forms.Label Lbl_Username;
+        private System.Windows.Forms.Button Btn_Back;
+    }
+}
